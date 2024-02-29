4f3364235fe6b0a58227b001b1a31c6bd77c0c4a
Shadab-Afzal7800 <shadabafzal7800@gmail.com>
2024-02-29 18:00:01 +0530

    refracted code

---
+++ lib/components/promoCard.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+Widget promoCard(image) {
+  return AspectRatio(
+    aspectRatio: 2.62 / 3,
+    child: Container(
+      margin: EdgeInsets.only(right: 15),
+      decoration: BoxDecoration(
+          color: Colors.orange,
+          borderRadius: BorderRadius.circular(20),
+          image:
+              DecorationImage(fit: BoxFit.cover, image: NetworkImage(image))),
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
+              0.1,
+              0.9
+            ], colors: [
+              Colors.black.withOpacity(.8),
+              Colors.black.withOpacity(.1)
+            ])),
+      ),
+    ),
+  );
+}
